42e302dcd75856f441d636fc409cd7c7e75595b4
“Nigamatullin <danichnigas@gmail.com>
2023-05-02 19:04:17 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 10000; // цена билета
+        int miles = 20; // миль
+        int bonus = (price / miles); // бонусные мили
+        System.out.println("Бонусных миль=" + (price / miles));
+
+    }
+}
+
